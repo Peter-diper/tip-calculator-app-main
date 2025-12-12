8e0b823678f5fd9c9031253120166d155f830b45
Peter <161234440+Peter-diper@users.noreply.github.com>
2025-12-12 10:13:02 +0330

    fixing on input not calculating price

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,3 @@
-// nodes
 const form = document.querySelector("form");
 const tipValueSection = document.querySelector(".tip--wrapper");
 const tipsOptions = tipValueSection.querySelectorAll(".btn");
@@ -9,9 +8,21 @@ const tipPerPerson = document.querySelector("#tip-amount");
 const PricePerPerson = document.querySelector("#total--price");
 const errorInputMsg = document.querySelector(".error--message");
 const inputWrappers = document.querySelectorAll(".input--wrapper");
+const activeBtn = document.querySelector(".active");
+
+const inputs = [totalPepole, totalBill];
+console.log(inputs);
 
 window.addEventListener("DOMContentLoaded", () => {
   startForm(form);
+  calculateTotal();
+  resetBtn.addEventListener("click", (e) => {
+    tipPerPerson.textContent = "$0.00";
+    PricePerPerson.textContent = "$0.00";
+    resetBtn.classList.remove("btn--secondery");
+    totalPepole.value = "0";
+    totalBill.value = "0";
+  });
 });
 
 // functions
@@ -22,51 +33,93 @@ function startForm(form) {
   });
 }
 
+function calculateTotal() {
+  totalBill.addEventListener("input", (e) => {
+    let totalPersonValue = +totalPepole.value === 0 ? 1 : +totalPepole.value;
+
+    const totalBillValue = +e.target.value;
+    // validation
+
+    tipsOptions.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+
+    errorInputMsg.style.display = "none";
+
+    const totalPricePerPerson = totalBillValue / totalPersonValue;
+    //adding total tip per person and total price per person
+    tipPerPerson.textContent = `$0.00`;
+    PricePerPerson.textContent = `$${totalPricePerPerson.toFixed(2)}`;
+
+    isReset(resetBtn);
+  });
+  totalPepole.addEventListener("input", (e) => {
+    console.log(e.target.value);
+    const totalPersonValue = +totalPepole.value;
+    const totalBillValue = +totalBill.value;
+    // validation
+    const validation = {
+      tipValue:
+        (typeof totalPersonValue === "number" && totalPersonValue > 0) || NaN,
+      billValue:
+        (typeof totalBillValue === "number" && totalBillValue > 0) || NaN,
+    };
+
+    if (!validation.billValue || !validation.tipValue) {
+      inputWrappers.forEach((input) => {
+        // check if both are fales
+        if (!validation.billValue && !validation.tipValue) {
+          input.classList.add("input--failed");
+          errorInputMsg.style.display = "block";
+          return;
+        }
+
+        if (!validation.billValue) {
+          input.dataset.id === "people--section" &&
+            input.classList.remove("input--failed");
+          input.dataset.id === "bill" && input.classList.add("input--failed");
+        }
+
+        if (!validation.tipValue) {
+          input.dataset.id === "bill" &&
+            input.classList.remove("input--failed");
+          input.dataset.id === "people--section" &&
+            input.classList.add("input--failed");
+
+          errorInputMsg.style.display = "block";
+        }
+      });
+      return;
+    }
+    inputWrappers.forEach((input) => {
+      input.classList.remove("input--failed");
+    });
+
+    tipsOptions.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+
+    errorInputMsg.style.display = "none";
+
+    const totalPricePerPerson = totalBillValue / totalPersonValue;
+    //adding total tip per person and total price per person
+    tipPerPerson.textContent = `$0.00`;
+    PricePerPerson.textContent = `$${totalPricePerPerson.toFixed(2)}`;
+
+    isReset(resetBtn);
+  });
+}
+
 function handeler(e) {
   e.preventDefault();
-  const totalPersonValue = +totalPepole.value;
-  const totalBillValue = +totalBill.value;
-  // validation
-  const validation = {
-    tipValue: !!String(totalPersonValue).indexOf(0) || NaN,
-    billValue: !!String(totalBillValue).indexOf(0) || NaN,
-  };
-
-  if (!validation.billValue || !validation.tipValue) {
-    inputWrappers.forEach((input) => {
-      // check if both are fales
-      if (!validation.billValue && !validation.tipValue) {
-        input.classList.add("input--failed");
-        errorInputMsg.style.display = "block";
-        return;
-      }
-
-      if (!validation.billValue) {
-        input.dataset.id === "people--section" &&
-          input.classList.remove("input--failed");
-        input.dataset.id === "bill" && input.classList.add("input--failed");
-      }
-
-      if (!validation.tipValue) {
-        input.dataset.id === "bill" && input.classList.remove("input--failed");
-        input.dataset.id === "people--section" &&
-          input.classList.add("input--failed");
-
-        errorInputMsg.style.display = "block";
-      }
-    });
-    return;
-  }
-  inputWrappers.forEach((input) => {
-    input.classList.remove("input--failed");
-  });
-
-  errorInputMsg.style.display = "none";
-
-  isReset(resetBtn);
 }
 
 function calcAmount(e) {
+  tipsOptions.forEach((btn) => {
+    btn.value !== e.target.value && btn.classList.remove("active");
+  });
+  e.target.classList.add("active");
+
   const totalPersonValue = +totalPepole.value;
   const totalBillValue = +totalBill.value;
 
@@ -84,11 +137,4 @@ function calcAmount(e) {
 
 function isReset(resetBtn) {
   resetBtn.classList.add("btn--secondery");
-  resetBtn.addEventListener("click", (e) => {
-    tipPerPerson.textContent = "$0.00";
-    PricePerPerson.textContent = "$0.00";
-    resetBtn.classList.remove("btn--secondery");
-    totalPepole.value = "0";
-    totalBill.value = "0";
-  });
 }
